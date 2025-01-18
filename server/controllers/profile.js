162151a0160b844ec1b593b14283416a51210491
BuKeMod <Xpotion2@gmail.com>
2025-01-18 22:45:03 +0700

    send data api test

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -2,16 +2,15 @@ import renderError from "../utils/renderError.js";
 
 const createProfile = (req, res, next) => {
         try {
-                if (true) {
-                return renderError(401, 'Password invalid')
-                }
+                const { firstname, lastname, clerkid } = req.body
+                console.log(firstname, lastname, clerkid);
 
                 console.log('create Profile success');
                 res.json({ message: "create Profile success" })
         } catch (error) {
-            
+
                 next(error)
-              
+
         }
 }
 
